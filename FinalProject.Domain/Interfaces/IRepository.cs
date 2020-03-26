474e940e2a26fe3563a93777da9999c8b19ac5ae
daniellimabrito <daniellimabrito@gmail.com>
2020-03-25 20:19:40 -0700

    Added Domain Layer

---
+++ FinalProject.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FinalProject.Domain.Interfaces
+{
+    public interface IRepository<TEntity> : IDisposable where TEntity : class
+    {
+        void Add(TEntity obj);
+        TEntity GetById(Guid id);
+        IEnumerable<TEntity> GetAll();
+        void Update(TEntity obj);
+        void Remove(Guid id);
+        int SaveChanges();
+    }
+}
---
+++ FinalProject.Domain/Model/Operation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FinalProject.Domain.Model
+{
+    public class Operation
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public double ExecutionTime { get; set; }
+        public DateTime ExecutionDate  { get; set; }
+
+
+    }
+}
